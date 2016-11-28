4df344d88814efdb03a8519a945cd45aa150bdb7
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2016-11-28 13:37:00 +0100

    Remove unnecessary import

--- app/shared/views/home.js
+++ app/shared/views/home.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import Helmet from 'react-helmet'
 
 import Post from '../components/post'
 import AppApi from '../api/AppApi'
